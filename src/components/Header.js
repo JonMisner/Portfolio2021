94441c4cbef6cbddcb86545e4207575c617f652d
JonM <jonmisner86@gmail.com>
2021-04-07 20:09:52 -0500

    css updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
-import Logo from "../images/logo.png";
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
 
 function Header() {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
   return (
     <header className="header" id="myHeader">
-      <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+      <Navbar light className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
           <a href="#home" className="nav-link active navbar-brand">
             <h1 id="top-logo">JM</h1>
           </a>
 
-          <button
+          {/* <button
             className="navbar-toggler float-left"
             type="button"
             data-bs-toggle="collapse"
@@ -22,37 +27,41 @@ function Header() {
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
-          </button>
+          </button> */}
+        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+        <Collapse isOpen={!collapsed} navbar>
+        <Nav navbar>
           <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a href="#about" className="nav-link active nav-link">
+            {/* <ul className="navbar-nav"> */}
+              <NavItem className="nav-item">
+                <NavLink href="#about" className="nav-link active nav-link">
                   About
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="#project" className="nav-link activenav-link">
+                </NavLink>
+              </NavItem>
+              <NavItem className="nav-item">
+                <NavLink href="#project" className="nav-link active nav-link">
                   Projects
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="#contact" className="nav-link active nav-link">
+                </NavLink>
+              </NavItem>
+              <NavItem className="nav-item">
+                <NavLink href="#contact" className="nav-link active nav-link">
                   Contact
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
+                </NavLink>
+              </NavItem>
+              <NavItem className="nav-item active">
+                <NavLink
                   href="https://drive.google.com/file/d/1QGGNdvhHxktihxbq0X4RZS4llMOs6PqQ/view?usp=sharing"
                   target="_blank"
                   className="nav-link nav-item"
                 >
                   Resume
-                </a>
-              </li>
-            </ul>
+                </NavLink>
+              </NavItem>
           </div>
+          </Nav>
+          </Collapse>
         </div>
-      </nav>
+      </Navbar>
     </header>
   );
 }
