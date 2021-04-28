a90d5cd26234d4526ed151d521fc8422f6bb637f
JonM <jonmisner86@gmail.com>
2021-04-27 22:53:42 -0500

    deployment tweaks

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,68 +2,79 @@ import React from "react";
 import Container from "../components/Container.js";
 import Row from "../components/Row.js";
 import Col from "../components/Col.js";
-import '../App.css';
+import "../App.css";
 
 import github from "../icons/githubLogo5.png";
 import MySQL from "../icons/mysqlLogo.png";
 import node from "../icons/nodeLogo3.png";
-import passport from "../icons/passportjsLogo.png"
-import reactLogo from '../icons/reactLogo.png';
-import foundationLogo from '../icons/foundationLogo.png';
-import bulma from '../icons/bulmaLogo.png';
-import mongoDB from '../icons/mongo3Logo.png';
-import express from '../icons/expressLogo2.png';
+import passport from "../icons/passportjsLogo.png";
+import reactLogo from "../icons/reactLogo.png";
+import foundationLogo from "../icons/foundationLogo.png";
+import bulma from "../icons/bulmaLogo.png";
+import mongoDB from "../icons/mongo3Logo.png";
+import express from "../icons/expressLogo2.png";
 
 function About() {
-    return (
-       <div className = "body">
-           <section id="about">
-           <Container>
-        <Row>
-         
-        <Col size="sm-12" className="about">
-            <p><span className="slide-right-font"> About Me </span></p>
-          </Col>
+  return (
+    <div className="body">
+      <section id="about">
+        <Container>
           <Row>
-            <div className="col about-section-font">
-              {/* <h3 className="about-section-header">Learner</h3> */}
-              <p className="about-paragraphs">
-              A fresh full stack developer, I’m ready to create and develop a stunning UX in web applications. I studied at Northwestern University where I gained skills in several different web development technologies, with an emphasis on MERN .  My background is in the Service Industry (with my passion being wine and cocktails) and I can bring that work ethic over with me to this new field. With an emphasis on mobile friendly and responsive designs, my goal is to provide polished, sleek, and engaging web applications. Furthermore, I strive to make the UI approachable, and intuitively easy to use for even novice users. Group projects have been a highlight of my coding experience, and I look forward to collaborating with other talented developers and engineers..
+            <Col size="sm-12" className="about">
+              <p>
+                <span className="slide-right-font"> About Me </span>
               </p>
-            </div>
+            </Col>
+            <Row>
+              <div className="col about-section-font">
+                {/* <h3 className="about-section-header">Learner</h3> */}
+                <p className="about-paragraphs">
+                  A fresh full stack developer, I’m ready to create and develop
+                  a stunning UX in web applications. I studied at Northwestern
+                  University where I gained skills in several different web
+                  development technologies, with an emphasis on MERN . My
+                  background is in the Service Industry (with my passion being
+                  wine and cocktails) and I can bring that work ethic over with
+                  me to this new field. With an emphasis on mobile friendly and
+                  responsive designs, my goal is to provide polished, sleek, and
+                  engaging web applications. Furthermore, I strive to make the
+                  UI approachable, and intuitively easy to use for even novice
+                  users. Group projects have been a highlight of my coding
+                  experience, and I look forward to collaborating with other
+                  talented developers and engineers..
+                </p>
+              </div>
+            </Row>
           </Row>
-        </Row>
-    
-        <Row>
-       
-          <div className="col">
-          <img src={github} alt="icon" className="img-fluid"/>
-        </div>
-        <div className="col">
-          <img src={MySQL} alt="icon" className="img-fluid"/>
-        </div>
-        <div className="col">
-          <img src={node} alt="icon" className="img-fluid node"/>
-        </div>
-        <div className="col">
-          <img src={passport} alt="icon" className="img-fluid"/>
-        </div>
-        <div className="col">
-          <img src={foundationLogo} alt="icon" className="img-fluid"/>
-        </div>
-        <div className="col">
-          <img src={bulma} alt="icon" className="img-fluid bulma"/>
-        </div>
-        <div className="col">
-          <img src={mongoDB} alt="icon" className="img-fluid"/>
-        </div>
-        <div className="col">
-          <img src={express} alt="icon" className="img-fluid express"/>
-        </div>
-        <div className="col">
-          <img src={reactLogo} alt="icon" className="img-fluid"/>
-        </div>
-         {/* <Col size="sm-6" className="about-photo-text-align">
+          <Row>
+            <div className="col">
+              <img src={github} alt="icon" className="img-fluid" />
+            </div>
+            <div className="col">
+              <img src={MySQL} alt="icon" className="img-fluid" />
+            </div>
+            <div className="col">
+              <img src={node} alt="icon" className="img-fluid node" />
+            </div>
+            <div className="col">
+              <img src={passport} alt="icon" className="img-fluid" />
+            </div>
+            <div className="col">
+              <img src={foundationLogo} alt="icon" className="img-fluid" />
+            </div>
+            <div className="col">
+              <img src={bulma} alt="icon" className="img-fluid bulma" />
+            </div>
+            <div className="col">
+              <img src={mongoDB} alt="icon" className="img-fluid" />
+            </div>
+            <div className="col">
+              <img src={express} alt="icon" className="img-fluid express" />
+            </div>
+            <div className="col">
+              <img src={reactLogo} alt="icon" className="img-fluid" />
+            </div>
+            {/* <Col size="sm-6" className="about-photo-text-align">
             <p><span className="slide-right-font"> Web Developer</span></p>
             <p className="about-section-font thumbnail-margin">
               I am an aspiring full stack web developer, with focus on front end development. I love to study new code and
@@ -75,12 +86,11 @@ function About() {
             goal of mine is real estate. I am a part of the LGBTQIA+ community, I follow women
             in tech and I am always open to new adventures!</p>
           </Col> */}
-        </Row>
+          </Row>
         </Container>
-           </section>
-
-</div>
-    );
+      </section>
+    </div>
+  );
 }
 
-export default About
+export default About;
