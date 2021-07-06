a2fd65815cf67e1e41e4caf418f421ec922c5503
Jon Misner <jonmisner86@gmail.com>
2021-07-05 19:59:44 -0500

    loading message added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.js";
 import "./App.css";
+
 import Header from "./components/Header.js";
 import Home from "./pages/Home.js";
 import About from "./pages/About.js";
 import Projects from "./pages/Projects.js";
 import Contact from "./pages/Contact.js";
-// import Wrapper from "./components/Wrapper.js";
-// import Resume from "./components/resume";
-import { Jumbotron, Container } from "reactstrap"
+
+import { Container } from "reactstrap";
+// const Login = lazy(() => import("./pages/login"));
 
 function App() {
   return (
     <Router>
-     <Container className='container-fluid'>
-        <Header />
-  
-     
-        <Home />
-        <About />
-        <Projects />
-        <Contact />
-
-      </Container>
-
+      <Suspense fallback={<p>Loading...</p>}>
+        <Container className="container-fluid">
+          <Header />
+          <Home />
+          <About />
+          <Projects />
+          <Contact />
+        </Container>
+      </Suspense>
     </Router>
   );
 }
